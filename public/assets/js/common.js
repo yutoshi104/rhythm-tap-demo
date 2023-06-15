f88acff3838fdb25d95eb384feba493c08f1bf88
Yuto <64243257+yutoshi104@users.noreply.github.com>
2023-06-15 15:03:52 +0900

    acceleration

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -196,9 +196,9 @@ if(location.pathname.match(/acceleration/g)){
             let ay_elem = document.getElementById('ay');
             let az_elem = document.getElementById('az');
             if(ax_elem && ay_elem && az_elem){
-                ax_elem.innerHTML = ax;
-                ay_elem.innerHTML = ay;
-                az_elem.innerHTML = az;
+                ax_elem.innerHTML = String(ax);
+                ay_elem.innerHTML = String(ay);
+                az_elem.innerHTML = String(az);
             }else{
                 console.log('x: '+String(ax));
                 console.log('y: '+String(ay));
